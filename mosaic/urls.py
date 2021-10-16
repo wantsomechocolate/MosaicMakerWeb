60a64939a7f1e3f1f8216234b65e39fc9c46c14a
wantsomechocolate <wantsomechocolate@gmail.com>
2021-10-15 23:01:25 -0400

    starting to abstract display of mosaics

--- mosaic/urls.py
+++ mosaic/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<mosaic_name>/', views.mosaic_view, name = 'mosaic_view'),
+    #path('<mosaic_name>/', views.mosaic_view, name = 'mosaic_view'),
+    path('mosaic_detail/<mosaic_name>', views.mosaic_detail, name = 'mosaic_detail'),
 ]
--- mosaic/views.py
+++ mosaic/views.py
@@ -8,5 +8,9 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index. Made a change")
 
-def mosaic_view(request, mosaic_name):
-	return HttpResponse("Hello, you're trying to view: " + mosaic_name)
+#def mosaic_view(request, mosaic_name):
+#	return HttpResponse("Hello, you're trying to view: " + mosaic_name)
+
+def mosaic_detail(request,mosaic_name):
+	context = {"mosaic_name":mosaic_name}
+	return render(request, "mosaic_detail.html", context)
