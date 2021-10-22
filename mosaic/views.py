5e6a950a5fe7af26814eebdc5191098a4971fb97
James McGlynn <wantsomechocolate@gmail.com>
2021-10-21 22:04:48 -0400

    Finally produced a mosaic from the db via looping through context variables in the template

--- mosaic/views.py
+++ mosaic/views.py
@@ -26,6 +26,8 @@ def mosaic_detail(request,mosaic_name):
 	
 
 	mosaic = Mosaic.objects.filter(id=3)[0]
+	mosaic.h_range = range(mosaic.h_sections)
+	mosaic.w_range = range(mosaic.w_sections)
 	sections = Section.objects.filter(mosaic = mosaic.id)
 
 	pieces = []
@@ -33,12 +35,17 @@ def mosaic_detail(request,mosaic_name):
 	    pieces.append(section.piece)
 	pieces = list(set(pieces))
 
-	context = dict(mosaic = mosaic, sections = sections, pieces = pieces)
+
 
 	#context = { 'mosaic_name':mosaic_name,
 	#			'filenames': [	['landscape/0-0.png','landscape/0-1.png','landscape/0-2.png'],
 	#							['landscape/1-0.png','landscape/1-1.png','landscape/1-2.png'],
 	#							['landscape/2-0.png','landscape/2-1.png','landscape/2-2.png'], 	] 	}
 
+	pieces_array = [(h,[None for w in range(mosaic.w_sections)]) for h in range(mosaic.h_sections)]
+	for section in sections:
+		pieces_array[section.coordinates[0]][1][section.coordinates[1]] = (section.coordinates[0],section.coordinates[1],section.piece.img.url)
+
+	context = dict(mosaic = mosaic, sections = sections, pieces = pieces, pieces_array = pieces_array)
 
 	return render(request, "mosaic_detail.html", context)
